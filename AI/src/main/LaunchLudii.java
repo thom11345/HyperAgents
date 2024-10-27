8427d93f25a2622a9accc5458ec4f899af786fce
Nick Thompson <thom1135@flinders.edu.au>
2024-10-27 13:42:15 +1030

    load models

--- AI/src/main/LaunchLudii.java
+++ AI/src/main/LaunchLudii.java
@@ -1,19 +1,26 @@
 package main;
 
+import java.util.LinkedHashMap;
+
+import agents.utils.ModelUtils;
 import app.StartDesktopApp;
 import utils.CliWrapper;
 import utils.HyperAgentFactory;
 
 public class LaunchLudii {
 
-    public static void main(String args[]) {
+	public static void main(String args[]) {
 
-	HyperAgentFactory.registerHyperAgents();
+		HyperAgentFactory.registerHyperAgents();
 
-	if (args.length > 0) {
-	    CliWrapper.runCommand(args);
-	} else {
-	    StartDesktopApp.main(args);
+		// load model evaluators
+		ModelUtils.agentEvaluator.evaluate(new LinkedHashMap<String, Double>());
+		ModelUtils.heuristicEvaluator.evaluate(new LinkedHashMap<String, Double>());
+
+		if (args.length > 0) {
+			CliWrapper.runCommand(args);
+		} else {
+			StartDesktopApp.main(args);
+		}
 	}
-    }
 }
